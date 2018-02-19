936def84eccf9c5086ef4e013c696a7e20c1b0ae
Mikhail Faraponov <mikefaraponov@fortress-1.local>
2018-02-20 01:21:29 +0200

    Updated

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,18 +2,21 @@ const assert = require('assert');
 const UPDATE_BUILD_STATUS = `SELECT 100;`;
 const APPLICATION_JSON = 'application/json';
 
-function BusController(io, db, ch) {
-  return {
-    async notify(msg) {
-      assert.equal(APPLICATION_JSON, msg.properties.contentType);
-      const content = JSON.parse(msg.content);
-      await db.query(UPDATE_BUILD_STATUS, {
-        replacements: content,
-        type: db.QueryTypes.INSERT,
-      });
-      io.to(content.roomId).emit(content);
-      ch.ack(msg);
-    },
+class BusController {
+  constructor(io, db, ch) {
+    this.io = io;
+    this.db = db;
+    this.ch = ch;
+  }
+  async notify(msg) {
+    assert.equal(APPLICATION_JSON, msg.properties.contentType);
+    const content = JSON.parse(msg.content);
+    await db.query(UPDATE_BUILD_STATUS, {
+      replacements: content,
+      type: db.QueryTypes.INSERT,
+    });
+    io.to(content.roomId).emit(content);
+    ch.ack(msg);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const db = require('src/resources/mysql');
 const createAmqpConnection = require('src/resources/rabbit');
 const BusController = require('src/controlllers');
 
-(async function() {
+(async function main() {
   try {
     const conn = await createAmqpConnection();
     conn.on("error", console.error);
@@ -13,7 +13,7 @@ const BusController = require('src/controlllers');
     const bus = new BusController(io, db, ch);
     ch.prefetch(10);
     ch.assertQueue(q, {durable: false});
-    ch.consume(q, bus.notify);
+    ch.consume(q, bus.notify.bind(bus));
     async function onShutdown() {
       conn.close();
       db.close();
